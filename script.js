0833d6c9b16067b83ebe803b8795f554b5507bb4
mic793-2 <mic793.github@gmail.com>
2026-05-09 13:32:07 +0200

    correct if statement

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ const age = Number(prompt("Quanti anni hai?"));
 
 const priceKm = 0.21;
 let priceTicket = km * priceKm;
+let discountPrice = priceTicket;//inizializzo la variabile con il prezzo del biglietto, così se non ci sono sconti rimane invariato
 
 if(age < 18){
-    priceTicket = priceKm *0.8;
+    //discountPrice = priceTicket * 0.2;
+    //priceTicket = priceTicket - discountPrice;
+    discountPrice = priceTicket - (priceTicket * 0.2);//posso fare direttamente il calcolo senza creare una variabile per lo sconto
+    
 }else if(age > 65){
-    priceTicket = priceKm *0.6;
+   // priceTicket = priceTicket *0.6;
+    discountPrice = priceTicket - (priceTicket * 0.4);//posso fare direttamente il calcolo senza creare una variabile per lo sconto
 }
 
-console.log("Il prezzo del biglietto è: " + priceTicket.toFixed(2) + "€");
+console.log("Il prezzo del biglietto è: " + discountPrice.toFixed(2) + "€");//toFixed(2) serve per limitare il numero di decimali a 2, altrimenti potrebbe avere molti decimali e non sarebbe bello da vedere
 
-alert("Il prezzo del biglietto è: " + priceTicket.toFixed(2) + "€");
+alert("Il prezzo del biglietto è: " + discountPrice.toFixed(2) + "€");
